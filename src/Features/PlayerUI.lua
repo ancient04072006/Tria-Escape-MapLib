e40f8525a6536619ee405c2737a0e7922c47f08d
ancient04072006 <swatcombat11@gmail.com>
2023-04-22 18:09:34 -0600

    n

--- src/Features/PlayerUI.lua
+++ src/Features/PlayerUI.lua
@@ -37,7 +37,14 @@ function PlayerUI.new()
 	return self
 end
 
---- Loads a ScreenGUI object into the Players PlayerGui. The ScreenGUI object has to be defined in the script somewhere in order to access it in this function. It can have Frames, TextLabels, etc in it as long as the parent is a ScreenGUI.
+--[=[ 
+	
+	Loads a ScreenGUI object into the Players PlayerGui. The ScreenGUI object has to be defined in the script somewhere in order to access it in this function. It can have Frames, TextLabels, etc in it as long as the parent is a ScreenGUI. 
+
+	Example: 
+	later
+
+]=]
 function PlayerUI:LoadUI(gui: ScreenGui): ScreenGui
 	assert(gui:IsA("ScreenGui"), "':LoadUI' must be passed a 'ScreenGUI'")
 
