aea7b30ceac1fe9958601d8f1e48133f73cca593
ancient04072006 <swatcombat11@gmail.com>
2023-04-22 18:05:23 -0600

    n

--- src/Features/PlayerUI.lua
+++ src/Features/PlayerUI.lua
@@ -13,7 +13,7 @@ local PlayerUI = { context = "client" }
 PlayerUI.__index = PlayerUI
 
 --- @class PlayerUI
---- Creates a new PlayerUI class
+--- Creates a new PlayerUI class for loading ScreenGUIS! This can be used for creating cutscenes, ending screens, etc.
 --- @client
 
 --- @prop cleanup {ScreenGUI}
@@ -37,7 +37,7 @@ function PlayerUI.new()
 	return self
 end
 
---- Loads a ScreenGUI object into the Players PlayerGui
+--- Loads a ScreenGUI object into the Players PlayerGui. The ScreenGUI object has to be defined in the script somewhere in order to access it in this function. It can have Frames, TextLabels, etc in it as long as the parent is a ScreenGUI.
 function PlayerUI:LoadUI(gui: ScreenGui): ScreenGui
 	assert(gui:IsA("ScreenGui"), "':LoadUI' must be passed a 'ScreenGUI'")
 
